8278299048a7c610b4128d6468838daa507e7e85
Aderick.k <aderick.k@gmail.com>
2016-05-05 17:00:33 +0700

    Edit the code to comply with Raspberry

--- Assignment 2/mcp_SevenSegmentLED.py
+++ Assignment 2/mcp_SevenSegmentLED.py
@@ -1,38 +1,38 @@
 import spidev
 import time
 import RPi.GPIO as GPIO
-import sevenSegmentLED.py
+import sevenSegmentLED as SSL
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(38, GPIO.OUT)
-GPIO.setup(40, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-InitializeSevenSegment()
+SSL.InitializeSevenSegment()
 
 while True:
         fromMCP = spi.xfer2([1, 8 << 4, 0])
         ADCvalue = ((fromMCP[1] & 3) << 8) + fromMCP[2]
         #print "ADC value = %d" % ADCvalue
-        str("ADC value = %d", ADCvalue)
+        #str("ADC value = %d", ADCvalue)
         
         per=(ADCvalue/1023.00)*100
-        #print "per = %d" % per
+        print "per = %d" % per
 
 
         if(per > 75):
-                GPIO.output(38,GPIO.HIGH)
-                GPIO.output(40,GPIO.LOW)
+                GPIO.output(7,GPIO.HIGH)
+                GPIO.output(8,GPIO.LOW)
         elif(per > 0 and per < 75):
-                GPIO.output(40,GPIO.HIGH)
-                GPIO.output(38,GPIO.LOW)
+                GPIO.output(8,GPIO.HIGH)
+                GPIO.output(7,GPIO.LOW)
         if(per == 0):
-                GPIO.output(38, GPIO.LOW)
-                GPIO.output(40, GPIO.LOW)
-        ShowNumber(per)
+                GPIO.output(7, GPIO.LOW)
+                GPIO.output(8, GPIO.LOW)
+        SSL.ShowNumber(per)
                 
         time.sleep(1)
 			
--- Assignment 2/sevenSegmentLED.py
+++ Assignment 2/sevenSegmentLED.py
@@ -1,9 +1,20 @@
 import RPi.GPIO as GPIO
+import time
         
 SevenSegmentPin = [31,32,33,35,36,37,38]
 
-LEDMapping = { ' ': PrintLED([0,0,0,0,0,0,0]),
-		'0': PrintLED([1,1,1,1,1,1,0]),
+def InitializeSevenSegment():
+        GPIO.setmode(GPIO.BOARD)
+        for x in range(0,7):
+                GPIO.setup(SevenSegmentPin[x], GPIO.OUT)
+        str("All lamp configured")
+	
+def PrintLED(lampStatus):
+        InitializeSevenSegment()
+	for x in range(0, 7):
+		GPIO.output(SevenSegmentPin[x], lampStatus[x])
+
+LEDMapping = { '0': PrintLED([1,1,1,1,1,1,0]),
 		'1': PrintLED([0,1,1,0,0,0,0]),
 		'2': PrintLED([1,1,0,1,1,0,1]),
 		'3': PrintLED([1,1,1,1,0,0,1]),
@@ -13,23 +24,14 @@ LEDMapping = { ' ': PrintLED([0,0,0,0,0,0,0]),
 		'7': PrintLED([1,1,1,0,0,0,0]),
 		'8': PrintLED([1,1,1,1,1,1,1]),
 		'9': PrintLED([1,1,1,1,0,1,1])}	
-
-def InitializeSevenSegment():
-        GPIO.setmode(GPIO.BOARD)
-        for x in range(0,7):
-                GPIO.setup(SevenSegmentPin[x], GPIO.OUT)
-        str("All lamp configured")
-				
+			
 def ShowNumber(perNumber):
-        if(perNumber < 10):
-                AssignLED(LEDMapping[0])
+        if(perNumber < 10.00):
+                LEDMapping['0']
         else:
-                requestedNumber = perNumber[:1]
-                AssignLED(LEDMapping[requestedNumber])
+                perString = str(perNumber)
+                requestedNumber = perString[:1]
+                LEDMapping[requestedNumber]
 				
-def AssignLED(lampStatus):
-        for x in range(0, 7):
-                GPIO.output(SevenSegmentPin[x], lampStatus[x])
-
 		
 
